5484d7b63f961b6d607202693d734348ea649db0
dmm <moshenskydenis@gmail.com>
2017-11-06 23:15:29 +0300

    release 1.0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mapipe',
-    version='1.0.2',
+    version='1.0.3',
     packages=['mapipe', 'mapipe.tools', 'mumapipe'],
     entry_points={
         'console_scripts': [
