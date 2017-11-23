49e7a0919663f119bac77c25457d30bc4422de9a
loven-doo <1429305+loven-doo@users.noreply.github.com>
2017-11-23 12:42:37 +0300

    Update __init__.py

--- mapipe/__init__.py
+++ mapipe/__init__.py
@@ -1,2 +1,1 @@
-from mapipe.mapipe_lib import get_counts, download_reads, filter_reads, map_reads, index_genome, calculate_counts, \
-    annotation_to_ensemble_gtf, gff_to_gtf
+from mapipe.mapipe_lib import get_counts, download_reads, filter_reads, map_reads, index_genome
