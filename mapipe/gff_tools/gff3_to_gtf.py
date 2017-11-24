fa5cbb45f596869500d1f18eec436a2418504e4f
loven-doo <moshenskydenis@gmail.com>
2017-11-24 23:42:42 +0300

    gff3_to_gtf fixed and it works now

--- mapipe/gff_tools/gff3_to_gtf.py
+++ mapipe/gff_tools/gff3_to_gtf.py
@@ -1,5 +1,7 @@
+import io
 import sys
 import argparse
+from collections import OrderedDict
 
 
 class GTFWriteTools(object):
@@ -22,10 +24,10 @@ class GTFWriteTools(object):
     @staticmethod
     def gtf_writer(gtf, s, new=False):
         if new:
-            gtf_f = open(gtf, 'w', newline="\n")
+            gtf_f = io.open(gtf, 'w', newline="\n")
         else:
-            gtf_f = open(gtf, 'a', newline="\n")
-        gtf_f.write(s + "\n")
+            gtf_f = io.open(gtf, 'a', newline="\n")
+        gtf_f.write(unicode(s + "\n"))
         gtf_f.close()
 
     def create_line(self):
@@ -44,14 +46,17 @@ class GTFWriteTools(object):
         col9_list = col9.split(";")
         for attr in col9_list:
             attr_list = attr.split("=")
-            if attr_list[0] in defined_attrs:
-                attr_dict[defined_attrs[attr]] = attr_list[1]
+            if attr_list[0] in defined_attrs.keys():
+                attr_dict[defined_attrs[attr_list[0]]] = attr_list[1]
         self.id = attr_dict['id']
         try:
             self.parent = parent_dict[attr_dict['parent']]
         except (KeyError, TypeError):
             self.parent = None
-        self.gene_name = attr_dict['gene_name']
+        try:
+            self.gene_name = attr_dict['gene_name']
+        except KeyError:
+            self.gene_name = "Undefined"
 
 
 class Gene(GTFWriteTools):
@@ -60,6 +65,7 @@ class Gene(GTFWriteTools):
         super(Gene, self).__init__(chr_name, source, start, end, score, strand, phase)
         self.feature = "gene"
         self.transcripts = []
+        self.exons = []
 
     def write_to_gtf(self, gtf):
         gene_line = self.create_line()
@@ -81,9 +87,12 @@ class Transcript(GTFWriteTools):
 
     def read_attr(self, col9, genes, id, parent, gene_name):
         super(Transcript, self).read_attr(col9, genes, id, parent, gene_name)
+        if not self.parent:
+            return False
         self.transcript_id = self.id
         self.gene_id = self.parent.gene_id
         self.gene_name = self.parent.gene_name
+        return True
 
     def write_to_gtf(self, gtf):
         transcript_line = self.create_line()
@@ -101,9 +110,12 @@ class Exon(GTFWriteTools):
 
     def read_attr(self, col9, transcripts, id, parent, gene_name):
         super(Exon, self).read_attr(col9, transcripts, id, parent, gene_name)
+        if not self.parent:
+            return False        
         self.gene_id = self.parent.gene_id
         self.gene_name = self.parent.gene_name
         self.transcript_id = self.parent.transcript_id
+        return True
 
     def write_to_gtf(self, gtf):
         exon_line = self.create_line()
@@ -120,10 +132,13 @@ class CDS(GTFWriteTools):
 
     def read_attr(self, col9, transcripts, id, parent, gene_name):
         super(CDS, self).read_attr(col9, transcripts, id, parent, gene_name)
+        if not self.parent:
+            return False
         self.gene_id = self.parent.gene_id
         self.gene_name = self.parent.gene_name
         self.transcript_id = self.parent.transcript_id
         self._define_exon()
+        return True
 
     def _define_exon(self):
         for exon in self.parent.exons:
@@ -143,25 +158,32 @@ class CDS(GTFWriteTools):
         self.gtf_writer(gtf, cds_line)
 
 
-def gff3_converter(gff, id_attrt='ID', parent_attr='Parent', gene_name_attr='Name', gtf=None, **kwargs):
-    features = ["gene", "transcript", "exon", "CDS"]
-    genes = {}
-    transcripts = {}
+def gff3_converter(gff, id_attrt='ID', parent_attr='Parent', gene_name_attr='gene', gtf=None, **kwargs):
+    genes = OrderedDict()
+    transcripts = OrderedDict()
+    header = True
     new_gtf = True
     if not gtf:
         gtf = ".".join(gff.split(".")[:-1]) + ".gtf"
-    for lines in gff:
+    gff_f = open(gff)
+    for lines in gff_f:
         line = None
         line = lines.strip()
         if len(line) == 0:
             continue
         if line[0] == "#":
-            GTFWriteTools.gtf_writer(gtf, line, new_gtf)
-            new_gtf = False
-        line_list = line.split()
-        if line_list[2] not in features:
+            if header:
+                GTFWriteTools.gtf_writer(gtf, line, new_gtf)
+                new_gtf = False
             continue
-        if line_list[2] == "gene":
+        line_list = line.split("\t")
+        try:
+            feature = None
+            feature = line_list[2].lower()
+        except IndexError:
+            continue
+        if feature == "gene":
+            header = False
             gene = Gene(
                 chr_name=line_list[0],
                 source=line_list[1],
@@ -173,7 +195,7 @@ def gff3_converter(gff, id_attrt='ID', parent_attr='Parent', gene_name_attr='Nam
             )
             gene.read_attr(line_list[8], id_attrt, parent_attr, gene_name_attr)
             genes[gene.gene_id] = gene
-        elif line_list[2][-3:] == "RNA":
+        elif "rna" in feature or "transcript" in feature:
             transcript = Transcript(
                 chr_name=line_list[0],
                 source=line_list[1],
@@ -183,10 +205,12 @@ def gff3_converter(gff, id_attrt='ID', parent_attr='Parent', gene_name_attr='Nam
                 strand=line_list[6],
                 phase=line_list[7]
             )
-            transcript.read_attr(line_list[8], genes, id_attrt, parent_attr, gene_name_attr)
+            attr_got = transcript.read_attr(line_list[8], genes, id_attrt, parent_attr, gene_name_attr)
+            if not attr_got:
+                continue
             transcript.parent.transcripts.append(transcript)
             transcripts[transcript.transcript_id] = transcript
-        elif line_list[2] == "exon":
+        elif feature == "exon":
             exon = Exon(
                 chr_name=line_list[0],
                 source=line_list[1],
@@ -196,9 +220,13 @@ def gff3_converter(gff, id_attrt='ID', parent_attr='Parent', gene_name_attr='Nam
                 strand=line_list[6],
                 phase=line_list[7]
             )
-            exon.read_attr(line_list[8], transcripts, id_attrt, parent_attr, gene_name_attr)
+            attr_got = exon.read_attr(line_list[8], transcripts, id_attrt, parent_attr, gene_name_attr)
+            if not attr_got:
+                attr_got = exon.read_attr(line_list[8], genes, id_attrt, parent_attr, gene_name_attr)            
+                if not attr_got:
+                    continue
             exon.parent.exons.append(exon)
-        elif line_list[2] == "CDS":
+        elif feature == "cds":
             cds = CDS(
                 chr_name=line_list[0],
                 source=line_list[1],
@@ -208,14 +236,18 @@ def gff3_converter(gff, id_attrt='ID', parent_attr='Parent', gene_name_attr='Nam
                 strand=line_list[6],
                 phase=line_list[7]
             )
-            cds.read_attr(line_list[8], transcripts, id_attrt, parent_attr, gene_name_attr)
+            attr_got = cds.read_attr(line_list[8], transcripts, id_attrt, parent_attr, gene_name_attr)
+            if not attr_got:
+                attr_got = cds.read_attr(line_list[8], genes, id_attrt, parent_attr, gene_name_attr)
+        else:
+            continue
     _write_genes(genes, gtf)
     return gtf
 
 
 def _write_genes(genes, gtf):
     for gene_id in genes.keys():
-        genes[gene_id].write_to_gtf()
+        genes[gene_id].write_to_gtf(gtf)
 
 
 def _parse_cmd_args(cmd_args):
@@ -246,9 +278,9 @@ def _parse_cmd_args(cmd_args):
     parser.add_argument(
         '-n',
         '--GeneName',
-        default="Name",
+        default="gene",
         required=False,
-        help="Equivalent to 'Name' attribute in your file",
+        help="Equivalent to 'gene' attribute in your file",
         action='store'
     )
     parser.add_argument(
