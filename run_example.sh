0c17d872a1bb10028a2364c470f707b3f8fa5439
loven-doo <1429305+loven-doo@users.noreply.github.com>
2017-11-25 22:58:49 +0300

    Update run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -1,5 +1,5 @@
-mapipe -r SRR3200454 -d SRRs -i ../Felis_catus/Genome_indices -a ../Felis_catus/GFF/ref_Felis_catus_8.0_top_level.gff3 -c config_example.ini
+mapipe -r SRR3200454 -d SRRs -i ../Felis_catus/Genome_indices -a ../Felis_catus/GFF/ref_Felis_catus_8.0_top_level.gtf -c config_example.ini
 
-mumapipe -l srr_list_example.txt -d SRRs -i ../Felis_catus/Genome_indices -a ../Felis_catus/GFF/ref_Felis_catus_8.0_top_level.gff3 -c mapipe/configs/config_example.ini -t 32  
+mumapipe -l srr_list_example.txt -d SRRs -i ../Felis_catus/Genome_indices -a ../Felis_catus/GFF/ref_Felis_catus_8.0_top_level.gtf -c mapipe/configs/config_example.ini -t 32  
 
 mapipe.gff_tools.gff3_to_gtf -i ref_Felis_catus_8.0_top_level.gff3
