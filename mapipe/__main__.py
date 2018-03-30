f5bf4187f12fa0eeda29333c8e208531b31b93bc
loven-doo <1429305+loven-doo@users.noreply.github.com>
2018-03-30 13:55:33 +0300

    Update __main__.py

--- mapipe/__main__.py
+++ mapipe/__main__.py
@@ -3,5 +3,5 @@ from mapipe import get_counts
 from mapipe.tools._inner_tools import _parse_cmd_args
 
 if __name__ == "__main__":
-    args = _parse_cmd_args(sys.argv[-1:])
+    args = _parse_cmd_args(sys.argv[1:])
     get_counts(*args)
